f0b586cf95165c692ad9ac63eaadb7947a7760f4
gerry leo nugroho <gerryleonugroho@gmail.com>
2020-08-10 08:53:14 +0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,87 +11,85 @@ const del = require('del');
 const env = process.env.NODE_ENV || 'prod';
 const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 const messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
 };
 
 var paths = {
-  styles: {
-    src: '_scss/*.scss',
-    dest: '_site/assets/css',
-    destsecond: 'assets/css',
-  },
-  scripts: {
-    src: 'assets/scripts/*.js',
-    dest: '_site/assets/scripts',
-  },
+    styles: {
+        src: '_scss/*.scss',
+        dest: '_site/assets/css',
+        destsecond: 'assets/css',
+    },
+    scripts: {
+        src: 'assets/scripts/*.js',
+        dest: '_site/assets/scripts',
+    },
 };
 
 /**
  * Build the Jekyll Site
  */
 function jekyllBuild() {
-  browserSync.notify(messages.jekyllBuild);
-  if (env === 'prod') {
-    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' });
-  } else {
-    return cp.spawn(
-      jekyll,
-      ['build', '--config', '_config.yml,_config.dev.yml'],
-      {
-        stdio: 'inherit',
-      },
-    );
-  }
+    browserSync.notify(messages.jekyllBuild);
+    if (env === 'prod') {
+        return cp.spawn(jekyll, ['build'], { stdio: 'inherit' });
+    } else {
+        return cp.spawn(
+            jekyll, ['build', '--config', '_config.yml,_config.dev.yml'], {
+                stdio: 'inherit',
+            },
+        );
+    }
 }
 
 function style() {
-  const processors = [
-    prefix({ browsers: ['> 5%', 'last 3 versions'] }),
-    cssnano,
-  ];
-  return gulp
-    .src('_scss/main.scss')
-    .pipe(
-      sass({
-        includePaths: ['scss'],
-        onError: browserSync.notify,
-      }),
-    )
-    .pipe(postcss(processors))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest(paths.styles.destsecond));
+    const processors = [
+        prefix({ browsers: ['> 5%', 'last 3 versions'] }),
+        cssnano,
+    ];
+    return gulp
+        .src('_scss/main.scss')
+        .pipe(
+            sass({
+                includePaths: ['scss'],
+                onError: browserSync.notify,
+            }),
+        )
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.reload({ stream: true }))
+        .pipe(gulp.dest(paths.styles.destsecond));
 }
 
 function reload(done) {
-  browserSync.reload();
-  done();
+    browserSync.reload();
+    done();
 }
 
 function browserSyncServe() {
-  browserSync.init({
-    server: {
-      baseDir: '_site',
-    },
-  });
+    browserSync.init({
+        server: {
+            baseDir: '_site',
+        },
+    });
 }
 
 function watch() {
-  gulp.watch(paths.styles.src, style);
-  gulp.watch(
-    [
-      '*.html',
-      '_layouts/*.html',
-      '_pages/*',
-      '_posts/*',
-      '_data/*',
-      '_includes/*',
-      'assets/scripts/*.js',
-      'assets/images/*.*',
-      // './**/*.md' // causes infinite loop
-    ],
-    gulp.series(jekyllBuild, reload),
-  );
+    gulp.watch(paths.styles.src, style);
+    gulp.watch(
+        [
+            '*.html',
+            '_layouts/*.html',
+            '_pages/*',
+            '_posts/*',
+            '_data/*',
+            '_includes/*',
+            'assets/scripts/*.js',
+            'assets/images/*.*',
+            // './**/*.md' // causes infinite loop
+        ],
+        gulp.series(jekyllBuild, reload),
+    );
 }
 
 /**
@@ -116,7 +114,7 @@ gulp.task('default', gulp.parallel(jekyllBuild, browserSyncServe, watch));
  * Deploy to GitHub Pages
  */
 function deploy() {
-  return gulp.src('./_site/**/*').pipe(ghpages());
+    return gulp.src('./_site/**/*').pipe(ghpages());
 }
 
 gulp.task('deploy', gulp.series(jekyllBuild, gulp.parallel(deploy)));
